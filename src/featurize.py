9b1d57a911d19003323162f37f8c676f59dbb36f
Hao Tian <haot@mail.smu.edu>
2022-05-02 13:20:17 -0500

    add PubChem features

--- src/featurize.py
+++ src/featurize.py
@@ -23,6 +23,7 @@ circular = deepchem.feat.CircularFingerprint()
 mol2vec = deepchem.feat.Mol2VecFingerprint()
 mordred = deepchem.feat.MordredDescriptors(ignore_3D=True)
 rdkit = deepchem.feat.RDKitDescriptors()
+pubchem = deepchem.feat.PubChemFingerprint()
 
 maccskeys_train_val = maccskeys.featurize(train_val.iloc[:, 1].to_list())
 maccskeys_test = maccskeys.featurize(test.iloc[:, 1].to_list())
@@ -34,21 +35,40 @@ mordred_train_val = mordred.featurize(train_val.iloc[:, 1].to_list())
 mordred_test = mordred.featurize(test.iloc[:, 1].to_list())
 rdkit_train_val = rdkit.featurize(train_val.iloc[:, 1].to_list())
 rdkit_test = rdkit.featurize(test.iloc[:, 1].to_list())
+pubchem_train_val = pubchem.featurize(train_val.iloc[:, 1].to_list())
+pubchem_test = pubchem.featurize(test.iloc[:, 1].to_list())
 
-# combine fingerprints
+# process pubchem empty lists
+pubchem_train_val_tmp = []
+for i in range(len(pubchem_train_val)):
+    if len(pubchem_train_val[i]) == 0:
+        pubchem_train_val[i] = np.array([0] * 881)
+    pubchem_train_val_tmp.append(pubchem_train_val[i].tolist())
+
+pubchem_test_tmp = []
+for i in range(len(pubchem_test)):
+    if len(pubchem_test[i]) == 0:
+        pubchem_test[i] = np.array([0] * 881)
+    pubchem_test_tmp.append(pubchem_test[i].tolist())
+
+pubchem_test = np.array(pubchem_test_tmp)
+
+# combine features
 fp_train_val = np.concatenate(
     (
-        maccskeys_train_val, circular_train_val,
-        mol2vec_train_val, rdkit_train_val, mordred_train_val
-    ), axis=1
-)
-fp_test = np.concatenate(
-    (
-        maccskeys_test, circular_test,
-        mol2vec_test, rdkit_test, mordred_test
+        maccskeys_train_val, circular_train_val, mol2vec_train_val,
+        rdkit_train_val, mordred_train_val, pubchem_train_val
     ), axis=1
 )
 
+fp_test = np.concatenate(
+    (
+        maccskeys_test, circular_test, mol2vec_test,
+        rdkit_test, mordred_test, pubchem_test
+    ), axis=1
+)
+
+# convert nan to 0
 fp_train_val = np.nan_to_num(fp_train_val, nan=0, posinf=0)
 fp_test = np.nan_to_num(fp_test, nan=0, posinf=0)
 
